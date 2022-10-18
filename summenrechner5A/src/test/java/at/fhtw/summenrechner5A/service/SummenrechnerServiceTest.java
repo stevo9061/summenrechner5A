0f88d7ccebd2e6ba77a6e0fa721ec0f31d564ecf
stevo9061 <sbittgen1@gmail.com>
2022-10-18 19:29:41 +0200

    Implement one test

--- summenrechner5A/src/test/java/at/fhtw/summenrechner5A/service/SummenrechnerServiceTest.java
+++ summenrechner5A/src/test/java/at/fhtw/summenrechner5A/service/SummenrechnerServiceTest.java
@@ -1,8 +1,26 @@
 package at.fhtw.summenrechner5A.service;
 
+import com.jayway.jsonpath.internal.function.numeric.Sum;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+// Erstellen einer Test Klasse:
 // Shift + Strg + T
 class SummenrechnerServiceTest {
 
+
+    // Benötigen eine Instanz von dem Service den wir testen möchten
+    private final SummenrechnerService sumService = new SummenrechnerService();
+
+    @Test
+    void testDefaultState() {
+        // Given/Arrange
+        // Zunächst Grundzustand angeben den wir haben möchten
+        int summand = 23;
+        // When/Act
+        sumService.incrementSumBy(summand);
+        // Then/Assert
+        assertEquals(summand, sumService.getSum());
+    }
 }
